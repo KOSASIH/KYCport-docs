01ccc1d990deb13b933f59626e50025ccc1f409f
KOSASIH <kosasihg88@gmail.com>
2024-07-21 16:06:27 +0700

    Create Documentation.js

---
+++ src/containers/Documentation.js
@@ -0,0 +1,13 @@
+import React from 'eact';
+import CodeEditor from '../components/CodeEditor';
+
+const Documentation = () => {
+  return (
+    <div>
+      <h1>Interactive Documentation</h1>
+      <CodeEditor />
+    </div>
+  );
+};
+
+export default Documentation;
