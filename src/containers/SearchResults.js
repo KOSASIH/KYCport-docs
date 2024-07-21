f3aea940ca794d85e127965c0f24c0ff314ff757
KOSASIH <kosasihg88@gmail.com>
2024-07-21 16:08:05 +0700

    Create SearchResults.js

---
+++ src/containers/SearchResults.js
@@ -0,0 +1,13 @@
+import React from 'eact';
+import SearchBar from '../components/SearchBar';
+
+const SearchResults = () => {
+  return (
+    <div>
+      <h1>Search Results</h1>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default SearchResults;
